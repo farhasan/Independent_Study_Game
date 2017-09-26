38fe9e8bbcbf73519f43ed959388caacb8b7ed46
Farhan Hasan <fih207@nyu.edu>
2017-09-26 09:45:53 -0400

    fixed visual studio issue

---
+++ DannyPhantomExample/DannyPhantomExample/sprite.h
@@ -0,0 +1,28 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <SDL.h>
+#include <string>
+
+using std::string;
+
+class Graphics;
+
+class Sprite
+{
+public:
+	Sprite();
+	Sprite(Graphics& graphics, const string &filePath, int sourceX, int sourceY, int width, int height, float posX, float posY);
+	virtual ~Sprite();
+	virtual void update();
+	void draw(Graphics& graphics, int x, int y);
+
+protected:
+	SDL_Rect _sourceRect;
+	SDL_Texture* _spriteSheet;
+
+private:
+	float _x, _y;
+};
+
+#endif
