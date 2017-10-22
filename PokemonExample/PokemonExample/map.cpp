784c0411dadbc53cf84ec0ed92da30b8a739c87e
Farhan Hasan <fih207@nyu.edu>
2017-10-22 01:28:26 -0400

    making test map

--- PokemonExample/PokemonExample/map.cpp
+++ PokemonExample/PokemonExample/map.cpp
@@ -7,23 +7,98 @@
 
 using std::vector;
 
+void Map::createPokeCenter(Map* map, Sprite* pokeCenter, int rowCoord, int colCoord)
+{
+	for (int row = rowCoord; row < rowCoord + 4; row += 4)
+	{
+		for (int col = colCoord; col < colCoord + 4; col += 4)
+		{
+			map->mapTiles[row][col] = pokeCenter;
+		}
+	}
+}
+
+void Map::createPokeMart(Map* map, Sprite* pokeMart, int rowCoord, int colCoord)
+{
+	for (int row = rowCoord; row < rowCoord + 4; row += 4)
+	{
+		for (int col = colCoord; col < colCoord + 4; col += 4)
+		{
+			map->mapTiles[row][col] = pokeMart;
+		}
+	}
+}
+
+void Map::createSmallTrees(Map* map, Sprite* smallTrees, int rowCoord, int numCols)
+{
+	for (int row = rowCoord; row < rowCoord + 4; row += 2)
+	{
+		for (int col = 0; col < numCols; col += 2)
+		{
+			map->mapTiles[row][col] = smallTrees;
+		}
+	}
+}
+
+void Map::createPath(Map* map, Sprite* leftTile, Sprite* middleTile, Sprite* rightTile, int rowCoord, int numCols)
+{
+	for (int row = rowCoord; row < rowCoord + 4; row += 4)
+	{
+		for (int col = 0; col < 2; col += 2)
+		{
+			map->mapTiles[row][col] = leftTile;
+		}
+	}
+
+	for (int row = rowCoord; row < rowCoord + 4; row += 4)
+	{
+		for (int col = 2; col < numCols - 2; col++)
+		{
+			map->mapTiles[row][col] = middleTile;
+		}
+	}
+
+	for (int row = rowCoord; row < rowCoord + 4; row += 4)
+	{
+		for (int col = numCols - 2; col < numCols; col += 2)
+		{
+			map->mapTiles[row][col] = rightTile;
+		}
+	}
+}
+
 Map* Map::createTestMap(Graphics& graphics) {
 	Map* map = new Map();
 
 	const int numRows = 45;
 	const int numCols = 80;
 
-	Sprite* sprite(new Sprite(graphics, "sprites/tileset.png", 16, 0, 16, 16, 0, 0));
+	Sprite* regularGrass(new Sprite(graphics, "sprites/tileset.png", 16, 0, 16, 16, 0, 0));
+	Sprite* smallTrees(new Sprite(graphics, "sprites/tileset.png", 96, 16, 32, 32, 0, 0));
+	Sprite* pokeCenter(new Sprite(graphics, "sprites/tileset.png", 0, 144, 64, 64, 0, 0));
+	Sprite* pokeMart(new Sprite(graphics, "sprites/tileset.png", 0, 80, 64, 64, 0, 0));
+	Sprite* leftEndPathTile(new Sprite(graphics, "sprites/tileset.png", 0, 560, 32, 48, 0, 0));
+	Sprite* middlePathTile(new Sprite(graphics, "sprites/tileset.png", 16, 560, 16, 48, 0, 0));
+	Sprite* rightEndPathTile(new Sprite(graphics, "sprites/tileset.png", 16, 560, 32, 48, 0, 0));
 
 	map->mapTiles = vector<vector<Sprite*>>(numRows, vector<Sprite*>(numCols));
 
-	for (int row = 0; row < numRows; row++)
+	
+	createSmallTrees(map, smallTrees, 0, numCols / 2);
+	createPokeCenter(map, pokeCenter, 4, 4);
+	createPath(map, leftEndPathTile, middlePathTile, rightEndPathTile, 10, numCols / 2);
+	createPokeMart(map, pokeMart, 14, 28);
+	
+	/*
+	for (int row = 4; row < 19; row++)
 	{
 		for (int col = 0; col < numCols; col++)
 		{
-			map->mapTiles[row][col] = sprite;
+			map->mapTiles[row][col] = regularGrass;
 		}
-	}
+	}*/
+
+	createSmallTrees(map, smallTrees, 19, numCols / 2);
 
 	return map;
 }
--- PokemonExample/PokemonExample/map.h
+++ PokemonExample/PokemonExample/map.h
@@ -15,6 +15,11 @@ public:
 
 	void update(int elapsedTime);
 	void draw(Graphics& graphics) const;
+	
+	static void createPokeCenter(Map* map, Sprite* pokeCenter, int rowCoord, int colCoord);
+	static void createPokeMart(Map* map, Sprite* pokeMart, int rowCoord, int colCoord);
+	static void createSmallTrees(Map* map, Sprite* tree, int rowCoord, int colCoord);
+	static void createPath(Map* map, Sprite* leftTile, Sprite* middleTile, Sprite* rightTile, int rowCoord, int numCols);
 private:
 	vector<vector<Sprite*>> mapTiles;
 };
